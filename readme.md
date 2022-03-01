e51718729a9f9f7cadda03027c07f1dc25e8ef70
WillySu <willhuntersu@yahoo.com>
2022-02-28 23:19:19 -0800

    Import glb file and add basic lighting

--- readme.md
+++ readme.md
@@ -5,3 +5,9 @@ Run `index.htm` in Web Browser
 Reference:
 
 - OrbitControls: https://threejs.org/docs/#examples/en/controls/OrbitControls
+- Limit on OrbitCOntrol: https://stackoverflow.com/questions/15827074/how-do-i-put-limits-on-orbitcontrol
+- Load 3D Models: https://threejs.org/docs/index.html#manual/en/introduction/Loading-3D-models
+- Load JSON: https://github.com/mrdoob/three.js/wiki/JSON-Object-Scene-format-4
+- Car model: https://www.turbosquid.com/3d-models/3d-model-stylized-cartoon-car-free-free-1778654
+- Free 3D House: https://3dexport.com/free-3dmodel-house-001-310345.htm
+- Load local file: https://stackoverflow.com/questions/8449716/cross-origin-requests-are-only-supported-for-http-but-its-not-cross-domain/14490982#14490982
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const UNIT = 16;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -7,19 +9,46 @@ const controls = new THREE.OrbitControls(camera, renderer.domElement);
 function resize () {
   const { innerWidth, innerHeight } = window
   camera.aspect = innerWidth / innerHeight;
-  camera.position.x = 64;
-  camera.position.y = -64;
-  camera.position.z = 64;
+  camera.position.x = UNIT;
+  camera.position.y = -UNIT;
+  camera.position.z = UNIT;
   camera.lookAt(0, 0, 0);
   camera.updateProjectionMatrix();
 
   controls.maxPolarAngle = Math.PI / 2;
   controls.update();
 
-  const geometry = new THREE.BoxGeometry(10, 10, 10);
+  /* const geometry = new THREE.BoxGeometry(10, 10, 10);
   const material = new THREE.MeshBasicMaterial({ wireframe: true });
   const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  scene.add(cube); */
+
+  /* const geometry = new THREE.PlaneGeometry(UNIT * 2, UNIT * 2);
+  const material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+  const plane = new THREE.Mesh(geometry, material);
+  plane.rotation.x = Math.PI / 2;
+  scene.add(plane); */
+
+  const geometry = new THREE.BoxGeometry(UNIT * 2, 1, UNIT * 2);
+  const material = new THREE.MeshLambertMaterial( {color: 0x004400, side: THREE.DoubleSide} );
+  const plane = new THREE.Mesh(geometry, material);
+  // plane.rotation.x = Math.PI / 2;
+  scene.add(plane);
+
+  /* const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+  scene.add(directionalLight); */
+
+  const spotLight = new THREE.SpotLight( 0xffffff );
+  spotLight.position.set(1, UNIT, UNIT);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+
+  const loader = new GLTFLoader();
+  loader.load('glb/House_001_GLB.glb', (gltf) => {
+    scene.add( gltf.scene );
+  }, undefined, (error) => {
+    console.error(error);
+  });
 
   renderer.setSize(innerWidth, innerHeight);
   renderer.render(scene, camera);
