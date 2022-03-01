b8bc13ad9293b99153147403e91afef57732a808
WillySu <willhuntersu@yahoo.com>
2022-02-28 21:27:10 -0800

    Add OrbitControls

---
+++ readme.md
@@ -0,0 +1,7 @@
+To Start:
+
+Run `index.htm` in Web Browser
+
+Reference:
+
+- OrbitControls: https://threejs.org/docs/#examples/en/controls/OrbitControls
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
 /** Resize canvas and camera */
 function resize () {
@@ -8,9 +9,12 @@ function resize () {
   camera.aspect = innerWidth / innerHeight;
   camera.position.x = 64;
   camera.position.y = -64;
-  camera.position.z = 128;
+  camera.position.z = 64;
   camera.lookAt(0, 0, 0);
   camera.updateProjectionMatrix();
+
+  controls.maxPolarAngle = Math.PI / 2;
+  controls.update();
 
   const geometry = new THREE.BoxGeometry(10, 10, 10);
   const material = new THREE.MeshBasicMaterial({ wireframe: true });
@@ -24,6 +28,14 @@ function resize () {
 function init () {
   resize();
   document.body.appendChild(renderer.domElement);
+  animate();
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	controls.update();
+	renderer.render(scene, camera);
 }
 
 document.addEventListener("DOMContentLoaded", init);
